983c1dccd32ed2c96a043fdf3928d3bd28d92bf0
zhu021 <zhu021@live.com>
2020-08-12 16:47:18 +0430

    a

--- ar3file/index.php
+++ ar3file/index.php
@@ -1,2 +1,7 @@
 <?php
-echo "xxxxxxx";
+function compare(array $array1 ,array $array2){
+    $array1=$array1;$array2=$array2;
+    print_r($array2);
+}
+$ali=["fasdfsdf"=>"fasdf"];
+compare($ali,$ali);
